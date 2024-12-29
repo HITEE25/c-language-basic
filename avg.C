2fbfb4098ecbfdd0e3757e06ebd6c81270f66fd3
HITEE25 <hitee0025@gmail.com>
2024-12-29 09:42:04 +0530

    Create avg.C

---
+++ avg.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,avg;
+    printf("enter value of a,b and c\n");
+    scanf("%d,%d,%d",a,b,c);
+    avg =(a+b+c)/3;
+    printf("the avg is %d", avg);
+    return 0;
+}
