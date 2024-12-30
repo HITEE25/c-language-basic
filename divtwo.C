a57b8ceb49d36fbc2da8078a0cb30a99906fccd0
HITEE25 <hitee0025@gmail.com>
2024-12-30 22:04:43 +0530

    Create divtwo.C

---
+++ divtwo.C
@@ -0,0 +1,11 @@
+//check number div by 2
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x;
+    printf("enter the value of x");
+    scanf("%d", &x);
+    printf("%d", x % 2 == 0);//output 1 div by 2 and if output 0 not div by 2
+    return 0;
+}
