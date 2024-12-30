32fcbd44bdfa0e6f8a6e0b964cf30f30f54c4f36
HITEE25 <hitee0025@gmail.com>
2024-12-30 22:06:58 +0530

    Create oddeven.C

---
+++ oddeven.C
@@ -0,0 +1,19 @@
+//to find odd or even
+#include<stdio.h>
+int main()
+{
+    int =n;
+    printf("enter the value of number");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("the number is even");
+    }
+    else
+    {
+        printf("the number is odd");
+    }
+    return 0;
+
+
+}
