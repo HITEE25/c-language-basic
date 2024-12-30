5666571d19f06984a5885f382229e1eedbf2e049
HITEE25 <hitee0025@gmail.com>
2024-12-30 21:47:37 +0530

    Create findvowel.C

---
+++ findvowel.C
@@ -0,0 +1,14 @@
+//to find character is vowel or consonant
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter the value of ch\n");
+    scanf("%c",&ch);
+    if(ch == 'a' ||ch == 'e' || ch == 'i' ||ch == 'o' ||ch == 'u' ||ch == 'A' ||ch == 'E' || ch == 'I' ||ch == 'O' ||ch == 'U' ){
+        printf("Vowels");
+    }
+    else{
+        printf("Cosonant");
+    }
+    return 0;
+}
